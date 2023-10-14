7c1e457ba18003c7d347aa1f398ee6be2e8abe51
Rafaela Pinter <rpinter811@gmail.com>
2023-10-13 23:15:32 -0400

    found valid neighbor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,6 +15,10 @@ public class Main {
 
         String filePath = args[0];
 
+        // Define characters to find
+        String firstCharacter = "e";
+        String secondCharacter = "v";
+
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             Puzzle puzzle;
 
@@ -25,16 +29,41 @@ public class Main {
                 puzzle.printPuzzle();
                 System.out.println("Words: " + puzzle.getWords());
 
-                // Testing finding character occurrences
-//                testFindOccurrences(puzzle, "a");
-                System.out.println(puzzle.getStringAtPosition(puzzle.findAllOccurrencesOfCharacter("e").top()));
-//                testFindOccurrences(puzzle, "e");
-//                testFindOccurrences(puzzle, "i");
+                // Find all occurrences of firstCharacter
+                ArrayStack<Position> firstCharacterPositions = puzzle.findAllOccurrencesOfCharacter(firstCharacter);
+
+                // If there is at least one occurrence of firstCharacter
+                if (!firstCharacterPositions.isEmpty()) {
+                    // Get the top position of firstCharacter
+                    Position firstCharacterPosition = firstCharacterPositions.top();
+                    System.out.println("Row " + firstCharacterPosition.getRow() + ", Column " + firstCharacterPosition.getColumn());
+
+                    System.out.println("Found a(n) '" + firstCharacter + "' at: Row " + firstCharacterPosition.getRow() + ", Column " + firstCharacterPosition.getColumn());
+
+                    // Find valid neighbors with secondCharacter from the position of firstCharacter
+                    ArrayStack<Position> secondCharacterNeighbors = puzzle.findValidNeighbors(firstCharacterPosition, secondCharacter);
+
+                    // Print the result
+                    System.out.println("Valid neighbors with '" + secondCharacter + "' around the first '" + firstCharacter + "':");
+
+                    while (!secondCharacterNeighbors.isEmpty()) {
+                        Position secondCharacterPosition = secondCharacterNeighbors.pop();
+                        System.out.println("Row " + secondCharacterPosition.getRow() + ", Column " + secondCharacterPosition.getColumn());
+                    }
+                } else {
+                    System.out.println("No '" + firstCharacter + "' found in the puzzle.");
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+                // Testing finding character occurrences
+//                testFindOccurrences(puzzle, "a");
+//                System.out.println(puzzle.getStringAtPosition(puzzle.findAllOccurrencesOfCharacter("e").top()));
+//                testFindOccurrences(puzzle, "e");
+//                testFindOccurrences(puzzle, "i");
+
 
     // Helper method to test finding character occurrences
     private static void testFindOccurrences(Puzzle puzzle, String character) {
